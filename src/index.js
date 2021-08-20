dea26e81192d4e4b0990600d12877f53afa38812
muhammadali23 <muhammadali121999@gmail.com>
2021-08-20 10:32:03 +0500

    first commit

--- src/index.js
+++ src/index.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+// import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
+const title = "I have to print Hello World";
+const print = React.createElement('h2',null,"Hello World");
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+ <>{title},{print}</>
+  ,document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
